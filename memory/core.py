426c89bbdec6fcac1bf2dab02ddbcbe99270f1b3
d74117157-create <d74117157@gmail.com>
2026-05-16 11:09:16 -0500

    Create core.pyfrom memory.db import init_db, save_task, update_task, get_stats

---
+++ memory/core.py
@@ -0,0 +1,11 @@
+def init_db():
+    print("init_db called")
+
+def save_task(task):
+    print("save_task called")
+
+def update_task(task_id, data):
+    print("update_task called")
+
+def get_stats():
+    return {"status": "ok"}
